21305a6bf3cdd4046c60d4758906d92c82426bdb
Tidykiwi <rosskeithbaker@gmail.com>
2021-04-14 16:26:47 +1200

    Added paintDottedLine method.

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,3 +1,5 @@
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
@@ -24,9 +26,19 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		
 	}
 	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create(); 
+			Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+			g2d.setStroke(dashed);
+			g2d.setPaint(Color.WHITE);
+			g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+			g2d.dispose();
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		paintDottedLine(g);
 	}
 	
 	@Override
