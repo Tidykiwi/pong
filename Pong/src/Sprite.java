a40f6c7be07bd6d1734fc8788957b72218bad487
Tidykiwi <rosskeithbaker@gmail.com>
2021-04-14 17:41:48 +1200

    Added code to keep sprite position within the bounds of the screen.

--- Pong/src/Sprite.java
+++ Pong/src/Sprite.java
@@ -1,9 +1,11 @@
+import java.awt.Color;
 
 public class Sprite {
 	
 	private int xPosition, yPosition;
 	private int xVelocity, yVelocity;
 	private int width, height; 
+	private Color colour;
 	
 	// Getter methods
 	public int getXPosition() {return xPosition;}
@@ -12,6 +14,7 @@ public class Sprite {
 	public int getyVelocity() {return yVelocity;}
 	public int getWidth() {return width;}
 	public int getHeight() {return height;}
+	public Color getColor() {return colour;}
 	
 	// Setter methods
 	public void setxPosition(int xPosition) {
@@ -32,5 +35,30 @@ public class Sprite {
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	public void setColor(Color colour) {
+		this.colour = colour;
+	}
+	
+	public void setXPosition(int newX, int panelWidth) {
+		xPosition = newX;
+		
+		if(xPosition < 0) {
+			xPosition = 0;
+		} else if(xPosition + width > panelWidth) {
+			xPosition = panelWidth - width;
+		}	
+	}
+	
+	public void setYPosiiton(int newY, int panelHeight) {
+		yPosition = newY;
+		
+		if(yPosition < 0) {
+			yPosition = 0;
+		} else if(yPosition + height > panelHeight) {
+			yPosition = panelHeight - height;
+		}		
+	}
+	
+	
 	
 }
